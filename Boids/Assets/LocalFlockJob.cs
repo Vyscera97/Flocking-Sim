203f99a11c8f0af55e2fbdca05063fae2f32b04a
Ben Baker <benbaker1997@gmail.com>
2023-07-28 15:46:52 -0700

    Probably Broken, but maybe working with jobs

--- Boids/Assets/LocalFlockJob.cs
+++ Boids/Assets/LocalFlockJob.cs
@@ -1,39 +1,60 @@
+using System;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 [BurstCompile]
-public struct LocalFlockJob : IJobParallelFor
+public struct LocalFlockJob : IJob
 {
+    public NativeArray<float2> newVelocity;
+
     [ReadOnly]
-    public NativeArray<float3> localFlockPositions;
+    public NativeArray<float2> localFlockPositions;
     [ReadOnly]
     public NativeArray<float2> localFlockVelocities;
-    public NativeArray<float3> newPos;
-    public NativeArray<float2> newVel;
-    public float minDistance;
-    //public float2 newVelocity;
-    public float2 avoidVelocity;
-    public float2 averageVelocity;
-    //public float2 avgVector;
-    public float3 avgPos;
-    public float3 pos;
 
-    public void Execute(int index)
-    {       
-        float3 difference = (localFlockPositions[index] - pos);
-        float distance = math.length(difference);
-        if (distance < minDistance)
+    [ReadOnly]
+    public float2 position;
+    [ReadOnly]
+    public float2 velocity;
+    [ReadOnly]
+    public float minDistance;    
+    [ReadOnly]
+    public float avoidFactor;
+    [ReadOnly]
+    public float alignFactor;
+    [ReadOnly]
+    public float cohereFactor;
+
+    public void Execute()
+    {
+        float localFlockCount = localFlockPositions.Length;
+
+        float2 avoidVelocity = float2.zero;
+        float2 averageVelocity = float2.zero;
+        float2 avgVector = float2.zero;
+        float2 avgPos = float2.zero;
+
+        for (int i = 0; i < localFlockPositions.Length; i++)
         {
-            avgPos += localFlockPositions[index];
-            averageVelocity += localFlockVelocities[index];
-            difference /= (distance * distance);
-            avoidVelocity -= distance;
+            float2 difference = (localFlockPositions[i] - position); ;
+            if (math.length(difference) < minDistance)
+            {
+                avgPos += localFlockPositions[i];
+                averageVelocity += localFlockVelocities[i];
+                difference /= math.lengthsq(difference);
+                avoidVelocity -= new float2(difference.x, difference.y);
+            }
         }
-        newPos[0] = avgPos;
-        newVel[0] = avoidVelocity;
-        newVel[1] = averageVelocity;
+
+        avoidVelocity = (avoidVelocity - velocity) * avoidFactor;
+        averageVelocity = ((averageVelocity / localFlockCount) - velocity) * alignFactor;
+        avgVector = (avgPos / localFlockCount) - position;
+        avgVector = (avgVector - velocity) * cohereFactor;
+        newVelocity[0] = (avoidVelocity + averageVelocity + avgVector);
+
     }
 }
