eaafc560c0fe77426e6647dfec85b796d2ce465e
Ben Baker <benbaker1997@gmail.com>
2023-07-30 16:57:42 -0700

    Better Behavior
    Also switched from using Vector2 to float2 for most calculations. In theory this is faster. However splitting the workload using Unity Jobs would still be vastly more efficient than running all calculations on the main thread

--- Boids/Assets/Boid.cs
+++ Boids/Assets/Boid.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Burst;
+using Unity.Mathematics;
 using UnityEngine.UIElements;
 
+[BurstCompile(CompileSynchronously = true)]
 public class Boid : MonoBehaviour
 {
     [SerializeField] float minDistance;
@@ -16,21 +19,22 @@ public class Boid : MonoBehaviour
     [SerializeField]
     List<GameObject> boids = new List<GameObject>();
 
-    Vector2 difference;
-    Vector2 velocity;
-    Vector2 newVelocity;
+    float2 difference;
+    float2 pos;
+    float2 velocity;
+    float2 newVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        Vector3 startingVector = Random.insideUnitCircle.normalized;
+        float2 startingVector = UnityEngine.Random.insideUnitCircle.normalized;
         newVelocity = startingVector * maxSpeed;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         Quaternion rotation = Quaternion.LookRotation(transform.forward, rb.velocity);
         transform.rotation = rotation;
         CheckEdge();
@@ -46,39 +50,40 @@ public class Boid : MonoBehaviour
     {
         velocity = newVelocity;
         newVelocity = Vector2.zero;
-        Vector3 pos = transform.position;        
-        Vector2 avoidVelocity = Vector2.zero;
-        Vector2 averageVelocity = Vector2.zero;
-        Vector2 avgVector = Vector2.zero;
-        Vector3 avgPos = Vector3.zero;
+        pos = new(transform.position.x, transform.position.y);
+        float2 avoidVelocity = Vector2.zero;
+        float2 averageVelocity = Vector2.zero;
+        float2 avgVector = Vector2.zero;
+        float2 avgPos = Vector2.zero;
         if (boids.Count > 0)
-        {            
+        {
             foreach (GameObject boid in boids)
             {
                 Boid Boid = boid.GetComponent<Boid>();
-                difference = (boid.transform.position - pos);
-                if (difference.magnitude < minDistance)
+                float2 otherPos = new(boid.transform.position.x, boid.transform.position.y);
+                difference = (otherPos - pos);
+                if (math.length(difference) < minDistance)
                 {
                     Rigidbody2D otherRb = boid.GetComponent<Rigidbody2D>();
                     averageVelocity += Boid.velocity;
-                    avgPos += boid.transform.position;
-                    difference /= difference.sqrMagnitude;
+                    avgPos += otherPos;
+                    difference /= math.lengthsq(difference);
                     avoidVelocity -= difference;
                 }
             }
-            avoidVelocity = (avoidVelocity) * avoidFactor;
+            avoidVelocity = (avoidVelocity - velocity) * avoidFactor;
             averageVelocity = ((averageVelocity / boids.Count) - velocity) * alignFactor;
             avgVector = avgPos / boids.Count - pos;
             avgVector = (avgVector - velocity) * cohereFactor;
             newVelocity = (avoidVelocity + averageVelocity + avgVector);
-        }  
-        newVelocity = (velocity + newVelocity).normalized * maxSpeed;
+        }
+        newVelocity = math.normalize(velocity + newVelocity) * maxSpeed;
     }
 
     void CheckEdge()
-    {        
+    {
         Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        
+
         if (pos.x <= 0.0f)
         {
             pos = new Vector3(1.0f, pos.y, pos.z);
